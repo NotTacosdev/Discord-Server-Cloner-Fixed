66b8b8db85ab70931452a22a0f18406c07a1cdbf
NotTacos <87448560+NotTacosdev@users.noreply.github.com>
2023-05-24 20:47:13 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,72 @@
+from os import system
+system("pip install discord==1.7.3")
+system("pip install discord-py==1.7.3")
+import psutil
+import time
+import discord
+import asyncio
+import ctypes
+import colorama
+from colorama import Fore, init, Style
+import platform
+from serverclone import Clone
+intents = discord.Intents().all()
+bot = discord.Client(intents=intents)
+os = platform.system()
+if os == "Windows":
+    system("cls")
+else:
+    system("clear")
+    print(chr(27) + "[2J")
+print(f"""{Fore.RED}
+
+                                    ██╗░░░░░██╗░░░██╗░█████╗░  ░█████╗░██╗░░░░░░█████╗░███╗░░██╗███████╗██████╗░
+                                    ██║░░░░░██║░░░██║██╔══██╗  ██╔══██╗██║░░░░░██╔══██╗████╗░██║██╔════╝██╔══██╗
+                                    ██║░░░░░██║░░░██║███████║  ██║░░╚═╝██║░░░░░██║░░██║██╔██╗██║█████╗░░██████╔╝
+                                    ██║░░░░░██║░░░██║██╔══██║  ██║░░██╗██║░░░░░██║░░██║██║╚████║██╔══╝░░██╔══██╗
+                                    ███████╗╚██████╔╝██║░░██║  ╚█████╔╝███████╗╚█████╔╝██║░╚███║███████╗██║░░██║
+                                    ╚══════╝░╚═════╝░╚═╝░░╚═╝  ░╚════╝░╚══════╝░╚════╝░╚═╝░░╚══╝╚══════╝╚═╝░░╚═╝
+{Style.RESET_ALL}
+                                                            {Fore.MAGENTA}Developed by: NotSaksh#6969.{Style.RESET_ALL}
+                                                            {Fore.MAGENTA}"Pycord sucks." - NotTacos 2023{Style.RESET_ALL}
+        """)
+ctypes.windll.kernel32.SetConsoleTitleW(f"Discord Server Cloner - SakshL and NotTacos")
+token = input(f'Please enter your token:\n >')
+guild_s = input('Please enter guild id you want to copy:\n >')
+guild = input('Please enter guild id where you want to copy:\n >')
+input_guild_id = guild_s  # <-- input guild id
+output_guild_id = guild  # <-- output guild id
+token = token  # <-- your Account token
+
+
+print("  ")
+print("  ")
+
+@bot.event
+async def on_ready():
+    print(f"Logged In as : {bot.user}")
+    print("Cloning Server")
+    guild_from = bot.get_guild(int(input_guild_id))
+    guild_to = bot.get_guild(int(output_guild_id))
+    await Clone.guild_edit(guild_to, guild_from)
+    await Clone.roles_delete(guild_to)
+    await Clone.channels_delete(guild_to)
+    await Clone.roles_create(guild_to, guild_from)
+    await Clone.categories_create(guild_to, guild_from)
+    await Clone.channels_create(guild_to, guild_from)
+    print(f"""{Fore.GREEN}
+
+
+                                            ░█████╗░██╗░░░░░░█████╗░███╗░░██╗███████╗██████╗░
+                                            ██╔══██╗██║░░░░░██╔══██╗████╗░██║██╔════╝██╔══██╗
+                                            ██║░░╚═╝██║░░░░░██║░░██║██╔██╗██║█████╗░░██║░░██║
+                                            ██║░░██╗██║░░░░░██║░░██║██║╚████║██╔══╝░░██║░░██║
+                                            ╚█████╔╝███████╗╚█████╔╝██║░╚███║███████╗██████╔╝
+                                            ░╚════╝░╚══════╝░╚════╝░╚═╝░░╚══╝╚══════╝╚═════╝░
+
+    {Style.RESET_ALL}""")
+    await asyncio.sleep(5)
+    await bot.close()
+
+
+bot.run(token, bot=False)
